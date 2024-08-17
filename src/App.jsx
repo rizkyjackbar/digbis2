2e229b279e3ca722673c514a9b64b465faa9736b
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-08-17 16:23:51 +0700

    add bgImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 // import React from 'react';
+import headerImg from '../src/assets/img/Header.png';
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen">
-        <div className="w-1/2 p-8">
-          <h1 className="text-3xl font-bold mb-4">Connect Your World: Empowering Innovators with IoTight Solutions</h1>
+      <div
+        className="flex items-start justify-start h-screen bg-cover bg-center"
+        style={{ backgroundImage: `url(${headerImg})` }}
+      >
+        <div className="w-1/2 p-8 bg-opacity-75 rounded">
+          <h1
+            className="mb-4"
+            style={{ fontFamily: 'Gelasio, serif', fontSize: '72px', textAlign: 'left', lineHeight: '1.1' }}
+          >
+            Connect Your World: Empowering Innovators with IoTight Solutions
+          </h1>
           <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
             Shop Now
           </button>
