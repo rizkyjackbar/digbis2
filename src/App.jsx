6a860698695f535923c085b1c8780e8885235799
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-08-17 15:43:05 +0700

    fix it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-function App() {
+// import React from 'react';
 
+function App() {
   return (
     <>
-      <div>
-        <h1>Hello World</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-1/2 p-8">
+          <h1 className="text-3xl font-bold mb-4">Connect Your World: Empowering Innovators with IoTight Solutions</h1>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            Shop Now
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
